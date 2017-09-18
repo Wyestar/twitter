437f688aba6eaad020e9de83152c12b6bf2c9dec
Eric Garnett <egcodes@gmail.com>
2017-09-18 19:36:38 -0400

    added nunjucks to render html

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
+
 
 app.use( (req, res, next) => {
   console.log('get is waiting');
@@ -9,13 +11,27 @@ app.use( (req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-  res.send('welcome');
-
+  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render('index', {title: 'Hall of Fame', people: people});
 })
 
 app.get('/news', (req, res) => {
   res.send('NEWS');
 })
+
+var locals = {
+  title: 'An Example',
+  people: [{name: 'Gandalf'}, {name: 'Frodo'}, {name: 'Hermione'}]
+};
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, (err, output) => {
+  console.log(output);
+});
+
+
 
 
 app.listen(3000, () => {
