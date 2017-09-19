dfc4c4c1af08099c28be8bfc6ca7780a2a7f6e4f
Doug Chin <dougmchin@gmail.com>
2017-09-19 15:30:31 -0400

    dynamicname

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,14 @@ const tweetBank = require('../tweetBank');
 router.get('/', (req, res) => {
   let tweets = tweetBank.list();
   // let names = tweetBank.find(tweetBank.list(), 'names: names');
-  res.render('index', { tweets: tweets, showForm: true});
+  res.render('index', { tweets: tweets, showForm: true, name: ""});
 })
 
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
+  console.log(name);
   var list = tweetBank.find( {name: name} );
-  res.render('index', { tweets: list, showForm: false });
+  res.render('index', { tweets: list, showForm: true, name: name });
 })
 
 router.get('/users/id/:id', function(req, res) {
